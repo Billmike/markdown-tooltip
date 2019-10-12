dc06211e70411179ba8926480a61a5945797897a
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-10-12 18:53:38 +0100

    Refactor code to move method for formating text to within tooltip component

--- src/tooltip/index.js
+++ src/tooltip/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import { markdownOptions } from '../utils/markdownOptions';
 import './tooltip.scss';
 
-export default function index({ formatText }) {
+export default function index({ formatText, selectedText, originalFullText }) {
+  /**
+   * 
+   * @param {String} markdownType The markdown type. Could be any of Bold, italics,
+   * quotes, etc
+   */
+  const markdownFormat = (markdownType) => {
+    let formattedString;
+    let formattedFullText;
+
+    switch (markdownType) {
+      case 'bold':
+        formattedString = `**${selectedText}**`;
+        break;
+      case 'italics':
+        formattedString = `*${selectedText}*`;
+        break;
+      case 'quotes':
+        formattedString = `>${selectedText}`;
+        break;
+      default:
+        break;
+    }
+    formattedFullText = originalFullText.replace(selectedText, formattedString);
+    formatText(formattedFullText)
+  }
+
   return (
     <div className="tool-tip-container">
       {
@@ -10,7 +36,7 @@ export default function index({ formatText }) {
           <button
             key={markdownType}
             className="tooltip-text"
-            onClick={() => formatText(markdownType)}
+            onClick={() => markdownFormat(markdownType)}
           >
             {icon}
           </button>
